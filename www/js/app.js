a2f093824b05f18544dd37438bdb04456ab5a1bd
operador <operador@debian>
2016-09-13 21:08:18 +0200

    * Dev day

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,40 @@
- 
- function initSystem() {
- 
-    console.log("fuuu")    
-     
- }
+
+var BACKEND = "https://dmz.cajaruraldelsur.es/ws/notiMovil/";
+var client = null;
+
+function Client() {
+    this.token = null;
+    this.backend = BACKEND;
+}
+
+Client.prototype.connect = function () {
+
+    $.ajax({
+        type: "POST",
+        url: BACKEND,
+        crossDomain: true,
+        beforeSend: function () { $.mobile.loading('show') },
+        complete: function () { $.mobile.loading('hide') },
+        data: { "TOKEN": this.token },
+        dataType: 'json',
+        success: function (e) {
+            if (e.status == "KO")
+                console.log(e);
+            else
+                console.log(e);
+        },
+        error: function () {
+           console.error("error");
+        }
+    });
+}
+
+
+function initSystem() {
+
+    console.log("Starting Core");
+    client = new Client();
+    client.connect();
+
+
+}
