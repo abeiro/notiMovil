b4f48fb73cebab0be93184dacaabc286c2a1d706
Augusto Beiro <augusto.beiro@gmail.com>
2016-09-14 15:07:20 +0200

    * Meh

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,11 @@
-
 var BACKEND = "https://dmz.cajaruraldelsur.es/ws/notiMovil/";
 var client = null;
 
 function Client() {
     this.token = null;
     this.backend = BACKEND;
+	this.token = window.localStorage.getItem("AUTHTOKEN");
+	
 }
 
 Client.prototype.connect = function () {
@@ -18,8 +19,11 @@ Client.prototype.connect = function () {
         data: { "TOKEN": this.token },
         dataType: 'json',
         success: function (e) {
-            if (e.status == "KO")
+            if (e.status == "KO") {
                 console.log(e);
+				$("body").pagecontainer("change", "#login");
+                
+			}
             else
                 console.log(e);
         },
@@ -29,6 +33,32 @@ Client.prototype.connect = function () {
     });
 }
 
+Client.prototype.login = function (user,password) {
+
+    $.ajax({
+        type: "POST",
+        url: BACKEND,
+        crossDomain: true,
+        beforeSend: function () { $.mobile.loading('show') },
+        complete: function () { $.mobile.loading('hide') },
+        data: { "USER": user ,"PASSWORD":password,"COMMAND":"LOGIN"},
+        dataType: 'json',
+        success: function (e) {
+            if (e.status == "KO") {
+                console.log(e);
+			}
+            else {
+				this.token=e.token;
+				window.localStorage.setItem("AUTHTOKEN",e.token);
+				$("body").pagecontainer("change", "#index");
+                console.log(e);
+			}
+        },
+        error: function () {
+           console.error("error");
+        }
+    });
+}
 
 function initSystem() {
 
@@ -36,5 +66,9 @@ function initSystem() {
     client = new Client();
     client.connect();
 
+	/* Listeners */
+	$('#loginButton').click(function() { 
+		client.login($("#user").val(),$("#password").val());
+	});
 
 }
